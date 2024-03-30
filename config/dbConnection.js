db64360499640d4017a77b0fe68c2aaeff21fa40
Vidhan27 <117517355+Vidhan27@users.noreply.github.com>
2024-03-30 23:37:28 +0530

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB= () => {
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Connected!')); 
 };
 
